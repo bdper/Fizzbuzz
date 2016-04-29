fe6ada410d85d59c8c2a2e4765678c0007d32e38
bdper <bdp007@aol.com>
2016-04-29 01:53:17 -0400

    First commit

---
+++ spec/fizzbuzz_spec.js
@@ -0,0 +1,28 @@
+var fb = require('../fizzbuzz');
+
+describe("a fizzbuzz program", function() {
+
+	it("can count from 1 to 5 and do fizzbuzz replacements", function() {
+		expect(fb.count(1,5)).toBe("1 2 fizz 4 buzz");
+		expect(fb.count(10,15)).toBe("buzz 11 fizz 13 14 fizzbuzz");
+	});
+
+	it("Can take a number and return 'fizz' if it is a multiple of 3, and the number otherwise", function() {
+		expect(fb.fizzer(3)).toBe('fizz');
+		expect(fb.fizzer(27)).toBe('fizz');
+		expect(fb.fizzer(5)).toBe(5);
+		expect(fb.fizzer(17)).toBe(17);
+	});
+	it("Can take a number and return 'buzz' if it is a multiple of 5, and the number otherwise", function() {
+		expect(fb.buzzer(5)).toBe('buzz');
+		expect(fb.buzzer(25)).toBe('buzz');
+		expect(fb.buzzer(3)).toBe(3);
+		expect(fb.buzzer(27)).toBe(27);
+	});
+	it("Can take a number and return 'fizzbuzz' if it is a multiple of 5 and 3, and the number otherwise", function() {
+		expect(fb.fizzbuzzer(15)).toBe('fizzbuzz');
+		expect(fb.fizzbuzzer(45)).toBe('fizzbuzz');
+		expect(fb.fizzbuzzer(3)).toBe(3);
+		expect(fb.fizzbuzzer(27)).toBe(27);	
+	});
+});
